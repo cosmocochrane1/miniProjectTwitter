013d8dd356079ae4ff1d0f6439f125d64b4ffe6b
Naina Prasad <prasad.naina@gmail.com>
2015-08-20 15:36:18 -0400

    added gems

--- app/models/sentiment.rb
+++ app/models/sentiment.rb
@@ -5,9 +5,9 @@ class SENTIMENT
 	Sentimental.load_defaults
 	Sentimental.threshold = 0.0
 
-	def check_sentiment(term)
+	def self.check_sentiment(term)
 		analyzer = Sentimental.new
-		score = analyzer.get_score term
+		score = analyzer.get_score(term)
 		return score
 	end
 end
