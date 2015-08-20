205af6e06ad70fb217960933e52c037f2abd45cf
Cosmo Cochrane <cosmocochrane1@gmail.com>
2015-08-20 15:40:55 -0400

    added twitter.rb to initializer, made twitter model

--- app/controllers/sentiments_controller.rb
+++ app/controllers/sentiments_controller.rb
@@ -1,4 +1,12 @@
 class SentimentsController < ApplicationController
+
+	def twittersearch
+		tweets = Twitter.twitter_search(params[:tweet])
+		SENTIMENT.check_sentiment(tweets)
+
+	end
+
+
 	def search
 		score = SENTIMENT.check_sentiment(params[:tweet])
 		render json: score 
--- app/models/twitter.rb
+++ app/models/twitter.rb
@@ -2,6 +2,17 @@ class Twitter < ActiverRecord::Base
 
 	def self.twitter_search(start_date, end_date, word)
 
+		require 'twitter'
+
+		client = Twitter::REST::Client.new do |config|
+		  config.consumer_key         = ENV['CONSUMER_KEY']
+		  config.consumer_secret      = ENV['CONSUMER_SECRET']
+		  config.access_token         = ENV['YOUR_ACCESS_TOKEN']
+		  config.access_token_secret  = ENV['YOUR_ACCESS_SECRET']
+		end 
+
+
+    client.get('https://twitter.com/search.json?q=flight%20%3A%28&src=typd')
 
 	end
 
