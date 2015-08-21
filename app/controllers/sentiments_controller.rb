f8f89238416ac48c1dfe69d272f135cac3e85831
Cosmo Cochrane <cosmocochrane1@gmail.com>
2015-08-21 16:56:37 -0400

    getting current time and date

--- app/controllers/sentiments_controller.rb
+++ app/controllers/sentiments_controller.rb
@@ -1,16 +1,18 @@
 class SentimentsController < ApplicationController
 
 	def twittersearch
-		tweets = Tweet.twitter_search(params[:tweet])
+		todays_date = Tweet.date_finder(168)
+		tweets = Tweet.twitter_search(params[:tweet], todays_date)
 		render json: tweets
-
 		Sentiment.check_sentiment(tweets)
+		
 
 	end
 
 
 	def search
+		data_set = 
 		score = Sentiment.check_sentiment(params[:tweet])
-		render json: score 
+		score 
 	end
 end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,7 +2,7 @@
 
 class Tweet 
 
-	def self.twitter_search(word)
+	def self.twitter_search(word, start_date, end_date)
 
 		client = Twitter::REST::Client.new do |config|
 		  config.consumer_key         = ENV['CONSUMER_KEY']
@@ -10,10 +10,21 @@ class Tweet
 		  config.access_token         = ENV['YOUR_ACCESS_TOKEN']
 		  config.access_token_secret  = ENV['YOUR_ACCESS_SECRET']
 		end 
+# - 3 weeks, and then -2 weeks, -1 week
+   		# client.get('https://twitter.com/search?q='+ word +'%20until%3A'+ start_date +'%' + word + '%20since%3Aend_date&src=typd')[:statuses]
+   		client.get('https://api.twitter.com/1.1/search/tweets.json?q=' + word + '&until=' + todays_date)[:statuses]
+	end
 
-   		client.get('https://api.twitter.com/1.1/search/tweets.json?q=' + word)[:statuses]
+	def self.date_finder(hours) 
+	
+		todays_date = DateTime.now 
+		todays_date = todays_date.to_s
+		todays_date = todays_date - (hours/24)
+		todays_date = todays_date[0..9]
+		hours =- 24
 
 	end
+
 
 
 end
