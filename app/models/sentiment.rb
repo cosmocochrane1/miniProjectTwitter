48a99a93e5e7f8ec60fefb93329ba9197ce77f04
Cosmo Cochrane <cosmocochrane1@gmail.com>
2015-08-22 19:32:10 -0400

    sentiment done

--- app/models/sentiment.rb
+++ app/models/sentiment.rb
@@ -6,17 +6,24 @@ class Sentiment
 	Sentimental.threshold = 0.0
 
 	def self.check_sentiment(array)
+		array_of_average_scores = []
 		sentiment_scores = []
 
-		array.each do |tweet| 
-			puts tweet.class
-			analyzer = Sentimental.new
-			score = analyzer.get_score(tweet[:text])
-			sentiment_scores.push(score)
+		array.each do |list_of_tweets|
+			list_of_tweets.each do |individual_tweet| 
+				puts individual_tweet.class
+				analyzer = Sentimental.new
+				score = analyzer.get_score(individual_tweet[:text])
+				sentiment_scores.push(score)
+			end
+			average_sentiment = sentiment_scores.inject{ |sum, value| sum + value }.to_f / sentiment_scores.size
+			array_of_average_scores.push(average_sentiment)
 		end
 		
-		average_sentiment = sentiment_scores.inject{ |sum, value| sum + value }.to_f / sentiment_scores.size
-		puts average_sentiment 
+
 
 	end
+
+
+
 end
