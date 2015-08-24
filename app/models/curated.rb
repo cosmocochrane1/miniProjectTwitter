f0df3d8b7f200768414e9404148f9d3cbe811c00
Naina Prasad <prasad.naina@gmail.com>
2015-08-24 16:47:11 -0400

    filled in details for curated.rb methods

--- app/models/curated.rb
+++ app/models/curated.rb
@@ -1,6 +1,12 @@
+# at the end of parsing thru the JSON object returned by one day's twitter call,
+# (iterate over an each loop of JSON results and do this function)
+# nyt_array_of_article_text will be full of strings that can be sent to 
+# sentiment checker / find average of all sentiments of all of today's articles
 require 'open-uri'
 class Curated
 	#for one day's twitter articles
+
+	#for FOX
 	def self.fox_articles(foxJSON)
 		fox_array_of_article_text = []
 		foxJSON.each do |one_tweet|
@@ -12,12 +18,48 @@ class Curated
 		end
 		return fox_array_of_article_text
 	end
+
+	#FOR NYT
+	def self.nyt_articles(nytJSON)
+		nyt_array_of_article_text = []
+		nytJSON.each do |one_tweet|
+			article_URI = one_tweet.entities.urls.url
+			page = Nokogiri::HTML(open(article_URI))
+			nyt_content = page.css('p.story-body-text')
+			nyt_content.join(" ")
+			nyt_array_of_article_text.push(nyt_content)
+		end
+		return nyt_array_of_article_text
+	end
+
+	#FOR GUARDIAN
+	def self.guardian_articles(guardianJSON)
+		guardian_array_of_article_text = []
+		guardianJSON.each do |one_tweet|
+			article_URI = one_tweet.entities.urls.url
+			page = Nokogiri::HTML(open(article_URI))
+			guardian_content = page.css('div.content__article-body p')
+			guardian_content.join(" ")
+			guardian_array_of_article_text.push(guardian_content)
+		end
+		return guardian_array_of_article_text
+	end
+
+	#FOR BLAZE
+	def self.blaze_articles(blazeJSON)
+		blaze_array_of_article_text = []
+		blazeJSON.each do |one_tweet|
+			article_URI = one_tweet.entities.urls.url
+			page = Nokogiri::HTML(open(article_URI))
+			blaze_content = page.css('div#postContent')
+			blaze_content.join(" ")
+			blaze_array_of_article_text.push(blaze_content)
+		end
+		return blaze_array_of_article_text
+	end
+
 end
 
 
 
 
-# at the end of parsing thru the JSON object returned by one day's twitter call,
-# (iterate over an each loop of JSON results and do this function)
-# nyt_array_of_article_text will be full of strings that can be sent to 
-# sentiment checker / find average of all sentiments of all of today's articles
