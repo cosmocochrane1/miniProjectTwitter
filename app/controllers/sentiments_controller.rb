a0b86f0d3d2a5180221e867dc5859a7c44b44c74
Naina Prasad <prasad.naina@gmail.com>
2015-08-24 13:52:56 -0400

    testing nokogiri

--- app/controllers/sentiments_controller.rb
+++ app/controllers/sentiments_controller.rb
@@ -6,13 +6,13 @@ class SentimentsController < ApplicationController
 		tweets = Tweet.twitter_search(params[:tweet])
 		render json: tweets
 		Sentiment.check_sentiment(tweets)
-		page = Nokogiri::HTML(open("http://en.wikipedia.org/"))   
-		puts "***********************************"
-		puts page
 	end
 
 	def fox_news
 		tweets = Tweet.fox_news_search(params[:tweet])
+		page = Nokogiri::HTML(open("http://t.co/XgWE1kD1Yc"))   
+		puts "***********************************"
+		puts page
 		render json: tweets
 
 	end
