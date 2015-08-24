afd5862b95bbc150437ec335efcc6007f0748a2c
Cosmo Cochrane <cosmocochrane1@gmail.com>
2015-08-24 15:31:43 -0400

    calcualting change in percentage sentiments.rb

--- app/controllers/sentiments_controller.rb
+++ app/controllers/sentiments_controller.rb
@@ -4,14 +4,13 @@ class SentimentsController < ApplicationController
 	def twittersearch
 		#binding.pry
 		tweets = Tweet.twitter_search(params[:tweet])
-		render json: tweets
-		Sentiment.check_sentiment(tweets)
+		Sentiment.check_sentiment_public(tweets)
 	end
 
 	def fox_news
-		tweets = Tweet.fox_news_search(params[:tweet])
-		page = Nokogiri::HTML(open("http://t.co/XgWE1kD1Yc"))   
-		puts "***********************************"
+		tweets = Tweet.fox_news_search(params[:tweet]) 
+		page = Nokogiri::HTML(open("http://t.co/XgWE1kD1Yc"))    
+		puts "***********************************" 
 		puts page
 		render json: tweets
 
--- app/models/sentiment.rb
+++ app/models/sentiment.rb
@@ -1,41 +1,48 @@
 require 'sentimental'
 
-class Sentiment 
-	# Load the default sentiment dictionaries
+class Sentiment
+  # Load the default sentiment dictionaries
 	Sentimental.load_defaults
 	Sentimental.threshold = 0.0
 
-	def self.check_sentiment(array)
-		array_of_average_scores = []
-		sentiment_scores = []
+  	def self.check_sentiment_public(array)
+    	array_of_average_scores = []
+    	sentiment_scores = []
 
-		array.each do |list_of_tweets|
-			list_of_tweets.each do |individual_tweet| 
-				puts individual_tweet.class
-				analyzer = Sentimental.new
-				score = analyzer.get_score(individual_tweet[:text])
-				sentiment_scores.push(score)
-			end
-			average_sentiment = sentiment_scores.inject{ |sum, value| sum + value }.to_f / sentiment_scores.size
-			array_of_average_scores.push(average_sentiment)
-		end
-		puts array_of_average_scores
-		binding.pry
-	end
+    	array.each do |list_of_tweets|
+      		list_of_tweets.each do |individual_tweet|
+        		analyzer = Sentimental.new
+        		score = analyzer.get_score(individual_tweet[:text])
+        		sentiment_scores.push(score)
+      		end
+      		average_sentiment = sentiment_scores.inject{ |sum, value| sum + value }.to_f / sentiment_scores.size
+      		array_of_average_scores.push(average_sentiment)
+      		# puts array_of_average_scores
+    	end
 
-	# [0.26444444444446663, 0.26057997557999996, 0.21464896214897777, 0.11160530237734999, 0.07321372908137332, 0.1180980811127889, 0.07898691955415237]
-
-
+    	array_of_average_scores.insert(0,0) #adds 0 as a value to the beginning of hte array
+    	percentage_change_array = []
+    	# i = 0
+    	# x = i+1
+    	# while i < array_of_average_scores.length - 1 do
+     #    	difference = array_of_average_scores[x] - array_of_average_scores[i]
+     #    	percent_change = (difference/array_of_average_scores[x])*100
+     #    	percentage_change_array.push(percent_change)
+     #    	i++
+    	#end
+    	#puts (percentage_change_array)
+    	i = 1
+    	array_of_average_scores.each_with_index{|item, i|
+    		difference = array_of_average_scores[i] - item
+    		percent_change = (difference/array_of_average_scores[i])*100
+    		percentage_change_array.push(percent_change)
+    		i++
+    	}
+  	end
+  	puts (percentage_change_array)
 end
 
-array_of_average_scores.insert(0,0)
-
-for (var i = 0;i<array_of_average_scores.lenght;i++) {
-	var difference = array_of_average_scores[i] - array_of_average_scores[i-1];
-	var percentage = difference/array_of_average_scores[i]
-}
-end
-
+  # [0.26444444444446663, 0.26057997557999996, 0.21464896214897777, 0.11160530237734999, 0.07321372908137332, 0.1180980811127889, 0.07898691955415237]
 
 
 
