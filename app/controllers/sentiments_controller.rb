655cb867277b83a315510a6c861941c757db42d6
Naina Prasad <prasad.naina@gmail.com>
2015-08-25 10:18:49 -0400

    modifying entire route thru app

--- app/controllers/sentiments_controller.rb
+++ app/controllers/sentiments_controller.rb
@@ -2,16 +2,17 @@ require 'open-uri'
 class SentimentsController < ApplicationController
 
 	def twittersearch
-		#binding.pry
-		tweets = Tweet.twitter_search(params[:tweet])
-		Sentiment.check_sentiment_public(tweets)
+		binding.pry
+		tweets = Tweet.twitter_search(params[:term])
+		var sent_array = Sentiment.check_sentiment_public(tweets)
+		return sent_array
 	end
 
-	def fox_news
-		tweets = Tweet.fox_news_search(params[:tweet]) 
-		render json: tweets
+	#def fox_news
+		#tweets = Tweet.fox_news_search(params[:tweet]) 
+		#render json: tweets
 
-	end
+	#end
 
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'application#index'
   post 'search' => 'sentiments#search'
-  post 'twittersearch' => 'sentiments#twittersearch'
+  get 'twittersearch' => 'sentiments#twittersearch'
   post 'fox_news' => 'sentiments#fox_news'
 end
